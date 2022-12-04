5b3a4c8fd0e0f05f2ae4a1246052631f375a3942
Niroshan2812 <niroshan9812@gmail.com>
2022-12-04 11:32:17 +0530

    user authenticate

--- message/App.tsx
+++ message/App.tsx
@@ -1,16 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import React, {useEffect} from 'react';
+import {StreamChat} from'stream-chat';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 // we use getstream.io to api --- api key
 const api_Key = 'kzs7gd4mqk8e'; 
+const client =StreamChat.getInstance(api_Key);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+useEffect(()=>{
+const connectUser = async ()=>{
+  await client.connectUser(
+    {
+      id:'niroshan',
+      name: 'Jim Lahey', 
+      image: 'https://i.imgur.com/fR9Jz14.png',
+    },
+    client.devToken('niroshan'),
+  );
+  console.log('User connected');
+};
+connectUser();
+},[])
+
 
   if (!isLoadingComplete) {
     return null;
@@ -20,6 +38,7 @@ export default function App() {
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
+    
     );
   }
 }
